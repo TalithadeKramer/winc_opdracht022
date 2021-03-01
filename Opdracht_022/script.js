0c6bb95207636665725a534164c8d20ee5ac1009
TalithadeKramer <76871148+TalithadeKramer@users.noreply.github.com>
2021-03-01 12:10:24 +0100

    Add files via upload

---
+++ Opdracht_022/script.js
@@ -0,0 +1,36 @@
+//defining a function
+/*const makeSpaghetti = function () {
+    console.log("Boil watter");
+    console.log("Add pasta");
+    console.log("Make sauce");
+    console.log("Drain pasta");
+    console.log("Combine pasta and sauce");
+};
+
+//calling a function
+
+makeSpaghetti();
+
+makeSpaghetti();*/
+
+
+const makePizza = function () {
+    console.log("Maak het deeg");
+    console.log("Rol het deeg uit");
+    console.log("Beleg het deeg met saus, salami, paprika, champignons en kaas");
+    console.log("Bak de pizza 12 minuten op 220 graden")
+};
+
+
+const makeSushi = function () {
+    console.log("Zoek een goede sushi zaak");
+    console.log("Bestel 12 tot 15 stuks sushi per persoon");
+    console.log("Geniet van de sushi");
+    console.log("Is de sushi niet wat je verwacht, bestel de volgende keer bij een andere sushi zaak");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
